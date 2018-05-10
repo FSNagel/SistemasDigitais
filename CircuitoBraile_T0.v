15161e2b04a31ab6c742289a97c28f27ef3cbb70
F.S. Nagel <33767134+FSNagel@users.noreply.github.com>
2018-05-09 21:46:12 -0300

    T0 - Braile

--- CircuitoBraile_T0.v
+++ CircuitoBraile_T0.v
@@ -1,43 +1,43 @@
-module phudim (
-	input [4:0] SW,
+module braile (
+	input [5:0] SW,
 	input [3:0] KEY,
 	output [6:0] HEX0,
 	output [6:0] HEX1,
 	output [6:0] HEX2,
 	output [6:0] HEX3
 );
-	reg [4:0] TEMP1;
-	reg [4:0] TEMP2;
-	reg [4:0] TEMP3;
-	reg [4:0] TEMP4;
+	reg [5:0] TEMP1;
+	reg [5:0] TEMP2;
+	reg [5:0] TEMP3;
+	reg [5:0] TEMP4;
 
-select S1(TEMP1[4:0], HEX0);
-select S2(TEMP2[4:0], HEX1);
-select S3(TEMP3[4:0], HEX2);
-select S4(TEMP4[4:0], HEX3);
+select S1(TEMP1[5:0], HEX0);
+select S2(TEMP2[5:0], HEX1);
+select S3(TEMP3[5:0], HEX2);
+select S4(TEMP4[5:0], HEX3);
 
 always@ (KEY)
 begin
 		
 	if(KEY[0] == 0)
 	begin
-		TEMP1[4:0] <= SW[4:0];
+		TEMP1[5:0] <= SW[5:0];
 		
 	end
 	
 	if(KEY[1] == 0)
 	begin
-		TEMP2[4:0] <= SW[4:0];
+		TEMP2[5:0] <= SW[5:0];
 	end
 	
 	if(KEY[2] == 0)
 	begin
-		TEMP3[4:0] <= SW[4:0];
+		TEMP3[5:0] <= SW[5:0];
 	end
 
 	if(KEY[3] == 0)
 	begin
-		TEMP4[4:0] <= SW[4:0];
+		TEMP4[5:0] <= SW[5:0];
 	end
 end		
 
@@ -46,7 +46,7 @@ end
 endmodule
 
 module select (
-	input [4:0] x,
+	input [5:0] x,
 	output [6:0] h
 );
 
@@ -55,7 +55,7 @@ assign h[0] = x == 3 || x == 25  || x == 19 || x == 10 || x == 5 || x == 7 || x 
 
 assign h[1] = x == 3 || x == 9 || x == 11  || x == 27 || x == 19 || x == 10 || x == 7 || x == 23 || x == 14 || x == 30;
 
-assign h[2] = x == 2 || x == 9 || x == 17  || x == 11 || x == 10 || x == 29 || x == 21 || x == 15 || x == 23 || x == 30 || x == 39 || x == 53;
+assign h[2] = x == 2 || x == 9 || x == 17  || x == 11 || x == 10 || x == 29 || x == 21 || x == 7 || x == 15 || x == 23 || x == 30 || x == 39 || x == 53;
 
 assign h[3] = x == 1  || x == 11 || x == 19 || x == 10 || x == 5 || x == 29 || x == 21 || x == 15 || x == 31 || x == 23 || x == 30 || x == 39 || x == 45 || x == 61;
 
